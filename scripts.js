de9b807a933ef5af0542faf86b90000d7db5b1d3
Nadia <68199213+knayad@users.noreply.github.com>
2021-11-03 19:16:19 -0400

    info()

--- scripts.js
+++ scripts.js
@@ -253,20 +253,18 @@ $('.multi-item-carousel .item').each(function(){
 
 // Script to hide/show menu
 
+function contactInfo() {
+  var x = document.getElementById("myInfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
-var button = document.querySelector('.click-to-toggle');
-
-button.addEventListener('click', function (event) {
-
-    var next = this.nextElementSibling;
-
-    if (next.style.display == "none") {
-        next.style.display = "block";
-
-    } else {
-        next.style.display = "none";
-    }
-});
+// //////////////////////// //
+   // end menu for phone //
+// ////////////////////// //
 
 // ///////////////////////// //
 //      LOADING CANVAS      //
