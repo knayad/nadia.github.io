3e48e3379ae13fb37f2480af3d4d5691ab5a994b
Nadia <68199213+knayad@users.noreply.github.com>
2021-11-03 14:16:35 -0400

    re-created phone visibility() to toggle

--- scripts.js
+++ scripts.js
@@ -246,15 +246,13 @@ $('.multi-item-carousel .item').each(function(){
 //      end  carousel    //
 // ///////////////////// //
 
-// //////////////////// //
-//  phone function     //
-// /////////////////// //
+// ///////////////////////// //
+// phone visibility function //
+// ///////////////////////// //
 
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
