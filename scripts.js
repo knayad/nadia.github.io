458cca57cb0f8912b536855ca7898959bea2dbbc
Nadia <68199213+knayad@users.noreply.github.com>
2021-11-03 14:26:53 -0400

    edited phone visibility()

--- scripts.js
+++ scripts.js
@@ -251,8 +251,6 @@ $('.multi-item-carousel .item').each(function(){
 // ///////////////////////// //
 
 function toggle_visibility(id) {
-       var e = document.getElementById(id);
-       if(e.style.display == 'block')
-          e.style.display = 'none';
-       else
-          e.style.display = 'block';
+var e = document.getElementById(id);
+e.style.display = ((e.style.display!='none') ? 'none' : 'block');
+}
